05ec367b2a2864e46df4d16ae8366aca27c08e53
Alejandro Barreto <alejandro5042@gmail.com>
2014-12-21 22:12:01 -0500

    Added ASP.NET project

---
+++ Studio/Controllers/AwesomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MightyFX.Studio.Controllers
+{
+    public class AwesomeController : ApiController
+    {
+        // GET: api/Awesome
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Awesome/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Awesome
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Awesome/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Awesome/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Studio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MightyFX.Studio.MvcApplication" Language="C#" %>
